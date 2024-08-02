57f840656e503b1a6842a72f96b26566dc4b2d28
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-02 15:09:12 -0700

    big o O(1)

---
+++ BigO/bigO_OhofOne.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+# O(1) ---> constant time, meaning as it increases the number of operations are going to remain constant
+def add_items(n):
+    return n + n + n
+
+
+
+add_items(2)
