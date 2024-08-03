8fd9be18086c3935cb9423e417c09a26cb58d898
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-03 08:26:15 -0700

    Big O complexity

--- BigO/bigO.py
+++ BigO/bigO.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 # Big O is always measuring worst case
-
+###############################################
+# if n = 1 BigO Values                        #
+# O(1)   =  1        --> Constant             # Good
+# O(log n) ~ 7       --> Divide and Conquer   # Good
+# O(n)   = 100       --> Proportional         # Bad
+# O(n^2) = 10,000    --> Loop within a Loop   # Horrible
+###############################################
+# To see bigO complexity charts https://www.bigocheatsheet.com/
 
 # O(n)
 def print_items(n):
