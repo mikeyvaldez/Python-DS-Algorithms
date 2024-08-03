019249cf59007b5b81d5c9e89e4b4e11bafd5332
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-03 09:42:02 -0700

    an example of a linked list class

---
+++ ClassesAndPointers/linkedListExample.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# This is an example of a linked list class
+
+class LinkiedList:
+    def __init__(self, value):      #   contructor
+    
+    def append(self, value):
+
+    def pop(self):
+
+    def prepend(self, value):
+
+    def insert(self, index, value):
+    
+    def remove(self, index):
