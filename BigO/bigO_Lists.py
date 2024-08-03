362f26db86b7b5b3967ee591072222848a8443ee
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-03 08:14:54 -0700

    quick overview of big o and lists

---
+++ BigO/bigO_Lists.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+
+# important notes about big O and lists
+# When removing or adding to the end of a list it will always be O(1)
+# When removing or adding to the front of a list it will be O(n).
+# This is because of the shifting or reindexing that has to be done in the list in,
+# in order to add or remove things from the front of a list.
